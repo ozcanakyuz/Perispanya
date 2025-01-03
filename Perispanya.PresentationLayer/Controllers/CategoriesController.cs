9af10953718b91a1325406fbd2e533cbba55040b
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-03 16:55:27 +0300

    CategoryList OK.

---
+++ Perispanya.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+
+namespace Perispanya.PresentationLayer.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return View(values);
+        }
+    }
+}
