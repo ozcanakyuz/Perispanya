e0499dc5e052942ea3e965676d7d122ffdd68551
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-03 15:22:41 +0300

    to create database

---
+++ Perispanya.DataAccessLayer/Context/PerispanyaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Perispanya.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perispanya.DataAccessLayer.Context
+{
+    public class PerispanyaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Initial Catalog=PerispanyaDb;Integrated Security=true");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
---
+++ Perispanya.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Perispanya.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
