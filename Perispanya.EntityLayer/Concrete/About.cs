dbce6f9d8d2340ee12962823ee8a31f0deacd620
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-03 14:58:03 +0300

    EntityLayer Concrete Folder Ok.

---
+++ Perispanya.EntityLayer/Concrete/About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perispanya.EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+    }
+}
