bacc59482e833128dd2d4608ca27396470ed5891
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-03 16:27:06 +0300

    _Head, _Sidebar, _MainPanelLayoutComponentPartial

---
+++ Perispanya.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_HeadLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perispanya.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _HeadLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Perispanya.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_MainPanelLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perispanya.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _MainPanelLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
