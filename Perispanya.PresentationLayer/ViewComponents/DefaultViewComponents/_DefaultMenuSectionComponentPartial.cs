33611976ada0b034feef79b71aa340200d63ccbe
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-05 00:02:33 +0300

    _DefaultMenuSectionComponentPartial

---
+++ Perispanya.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultMenuSectionComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+
+namespace Perispanya.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultMenuSectionComponentPartial : ViewComponent
+    {
+        public readonly IProductService _productService;
+
+        public _DefaultMenuSectionComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _productService.TGetAll();
+            return View(values);
+        }
+    }
+}
