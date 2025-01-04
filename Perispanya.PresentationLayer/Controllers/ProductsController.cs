38a052f183e5a58ae15712d590891e121fd6dbec
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-04 12:27:18 +0300

    ProductList Ok

---
+++ Perispanya.PresentationLayer/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+
+namespace Perispanya.PresentationLayer.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult ProductList()
+        {
+            var values = _productService.TGetAll();
+            return View(values);
+        }
+    }
+}
