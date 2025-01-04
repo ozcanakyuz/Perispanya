34187011fae4154879a5ba045b9f16b06cb339f3
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-05 00:05:19 +0300

    _DefaultFourcupSectionComponentPartial

---
+++ Perispanya.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultFourcupSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perispanya.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFourcupSectionComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
