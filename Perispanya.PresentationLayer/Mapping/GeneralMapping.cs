0c672a5f23f3e17dd97714cd13ae572161e46b5b
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-04 13:00:12 +0300

    Dtos Mapping General Mapping

---
+++ Perispanya.PresentationLayer/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Perispanya.DtoLayer.Dtos.AboutDtos;
+using Perispanya.DtoLayer.Dtos.SliderDtos;
+using Perispanya.EntityLayer.Concrete;
+
+namespace Perispanya.PresentationLayer.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<CreateAboutDto, About>().ReverseMap();
+            CreateMap<GetByIdAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+
+            CreateMap<ResultSliderDto, Slider>().ReverseMap();
+            CreateMap<CreateSliderDto, Slider>().ReverseMap();
+            CreateMap<GetByIdSliderDto, Slider>().ReverseMap();
+            CreateMap<UpdateSliderDto, Slider>().ReverseMap();
+        }
+    }
+}
