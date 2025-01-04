2ccbb2c6296d790a602fdae7bdc40493512cd7f7
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-04 13:07:23 +0300

    SliderList, CreateSlider, DeleteSlider, UpdateSlider DTO

--- Perispanya.PresentationLayer/Controllers/SlidersController.cs
+++ Perispanya.PresentationLayer/Controllers/SlidersController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+using Perispanya.DtoLayer.Dtos.SliderDtos;
+using Perispanya.EntityLayer.Concrete;
 
 namespace Perispanya.PresentationLayer.Controllers
 {
     public class SlidersController : Controller
     {
-        public IActionResult Index()
+        private readonly ISliderService _sliderService;
+        private readonly IMapper _mapper;
+
+        public SlidersController(ISliderService sliderService, IMapper mapper)
+        {
+            _sliderService = sliderService;
+            _mapper = mapper;
+        }
+
+        public IActionResult SliderList()
+        {
+            var values = _sliderService.TGetAll();
+            return View(_mapper.Map<List<ResultSliderDto>>(values));
+        }
+        [HttpGet]
+        public IActionResult CreateSlider()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateSlider(CreateSliderDto createSliderDto)
+        {
+            var value = _mapper.Map<Slider>(createSliderDto);
+            _sliderService.TInsert(value);
+            return RedirectToAction("SliderList");
+        }
+
+        public IActionResult DeleteSlider(int id)
+        {
+            _sliderService.TGetById(id);
+            return RedirectToAction("SliderList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSlider(int id)
+        {
+            var value = _sliderService.TGetById(id);
+            return View(_mapper.Map<GetByIdSliderDto>(value));
+        }
+        [HttpPost]
+        public IActionResult UpdateSlider(UpdateSliderDto updateSliderDto)
+        {
+            var value = _mapper.Map<Slider>(updateSliderDto);
+            _sliderService.TUpdate(value);
+            return RedirectToAction("SliderList");
         }
     }
 }
