73b4de6cb039e51b257ac9863c009351a3a124cf
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-04 13:56:51 +0300

    AboutList.cshtml ok

---
+++ Perispanya.PresentationLayer/Controllers/AboutsController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+using Perispanya.DtoLayer.Dtos.AboutDtos;
+
+namespace Perispanya.PresentationLayer.Controllers
+{
+    public class AboutsController : Controller
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+
+        public AboutsController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetAll();
+            return View(_mapper.Map<List<ResultAboutDto>>(values));
+        }
+    }
+}
