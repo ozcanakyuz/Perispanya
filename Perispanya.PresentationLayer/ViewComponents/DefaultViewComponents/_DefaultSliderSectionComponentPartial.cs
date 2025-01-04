fb399a60377f71a2d4d660e888f7544ed3c0191a
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-04 22:40:12 +0300

    _DefaultSliderSectionComponentPartial

---
+++ Perispanya.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultSliderSectionComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perispanya.BusinnesLayer.Abstract;
+
+namespace Perispanya.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSliderSectionComponentPartial : ViewComponent
+    {
+        private readonly ISliderService _sliderService;
+
+        public _DefaultSliderSectionComponentPartial(ISliderService sliderService)
+        {
+            _sliderService = sliderService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _sliderService.TGetAll();
+            return View(values);
+        }
+    }
+}
